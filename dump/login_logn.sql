7fcf7737a22e803a2b3f42c42ad5bcbecb94ed0a
4tushar7 <tushar07.sangwan@gmail.com>
2018-11-12 23:43:49 +0530

    onlinestudentmanagement

---
+++ dump/login_logn.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.5.60, for Win32 (AMD64)
+--
+-- Host: localhost    Database: login
+-- ------------------------------------------------------
+-- Server version	5.5.60
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `logn`
+--
+
+DROP TABLE IF EXISTS `logn`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `logn` (
+  `name` varchar(50) DEFAULT NULL,
+  `pwd` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logn`
+--
+
+LOCK TABLES `logn` WRITE;
+/*!40000 ALTER TABLE `logn` DISABLE KEYS */;
+INSERT INTO `logn` VALUES ('tushar07.sangwan@gmail.com','#gate1398'),('shubhamss.rana@gmail.com','#gate1367'),('Bhatianeeraj5@gmail.com','#gate1240'),('sparash29@gmail.com','#gate1381'),('rahul292@gmail.com','#gate1292'),('rahul29@gmail.com','12345'),('sahilAggarwall322@gmail.com','lalla'),('uppalVatsal.cse16@gmail.com','chitkara16'),('rashi99@gmail.com','1222');
+/*!40000 ALTER TABLE `logn` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-12 23:30:46
